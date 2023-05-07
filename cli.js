df005a00e9b79ca63d81e597cd6da38bb83a007e
Aidan Hunt <aidanhunt@acm.org>
2023-05-07 18:13:36 -0400

    most of the code

--- cli.js
+++ cli.js
@@ -23,3 +23,52 @@ if (args.h || args.help) {
    process.exit(0)
 }
 
+// get system timezone 
+const timezone = moment.tz.guess()
+const lat, lon
+
+// get arguments
+if (args.n) {
+  lat = args.n
+} else if (args.s) {
+  lat = -(args.s)
+} else {
+  console.log('Latitude (-n|-s) argument is required.')
+}
+
+if (args.e) {
+  lon = args.e
+} else if (args.w) {
+  lon = -(args.w)
+} else {
+  console.log('Longitude (-e|-w) argument is required.')
+}
+
+if (args.z) timezone = args.z
+
+// make request to API
+const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,sunrise,precipitation_sum,precipitation_hours&timezone=${timezone}`)
+
+const data = await response.json()
+
+// just print output if requested
+if (args.j) {
+  // pretty print JSON (per SO 5670752)
+  console.log(JSON.stringify(data, null, " ")
+  process.exit(0)
+}
+
+// otherwise, report rain boolean for chosen day
+
+console.log(`You ${data.daily.precipitation_hours[days] > 0 ? 'might' : 'will not'} need your galoshes `)
+
+switch (days) {
+  case 0:
+    console.log('today')
+    break;
+  case 1:
+    console.log('tomorrow')
+    break;  
+  default:
+    console.log(`in ${days} days.`)
+}
