5add1198c8ab7b795fa97ee85576784343de6db1
Aidan Hunt <aidanhunt@acm.org>
2023-05-07 17:47:16 -0400

    initial file

---
+++ cli.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+const args = require('minimist')(process.argv.slice(2))
+
+// check for help flag
+
+if (args.h || args.help) {
+  console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.`)
+}
+
